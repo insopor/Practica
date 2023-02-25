dbd7112de4ab9da59101489cb6e9ead14a17dae5
insopor <garturorf@gmail.com>
2023-02-25 14:16:01 -0600

    se hicieron algunas ofidicaciones en los metodos en el service

--- src/main/java/mx/com/practica/dao/IUsuarioDao.java
+++ src/main/java/mx/com/practica/dao/IUsuarioDao.java
@@ -1,11 +1,8 @@
 package mx.com.practica.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import mx.com.practica.modelos.Pizza;
-import mx.com.practica.modelos.Sabor;
-import mx.com.practica.modelos.Tamano;
 
 public interface IUsuarioDao {
 	
